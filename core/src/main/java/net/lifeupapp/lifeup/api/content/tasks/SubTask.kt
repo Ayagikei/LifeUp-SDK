1f8fc8da28ba8b62a01b119b4fb896ef3120201d
AyagiKei <kei.ayagi@gmail.com>
2026-05-25 20:43:08 +0800

    fix(tasks): 🐛 preserve subtask constructor compatibility

--- core/src/main/java/net/lifeupapp/lifeup/api/content/tasks/SubTask.kt
+++ core/src/main/java/net/lifeupapp/lifeup/api/content/tasks/SubTask.kt
@@ -10,11 +10,11 @@ data class SubTask(
     val todo: String,
     val status: Int,
     val remindTime: Long? = null,
-    val endTime: Long? = null,
     val exp: Int,
     val coin: Long? = null,
     val coinVariable: Long? = null,
     val items: List<RewardItem> = emptyList<RewardItem>(),
     val order: Int? = 0,
-    val autoUseItem: Boolean? = false
+    val autoUseItem: Boolean? = false,
+    val endTime: Long? = null
 )
